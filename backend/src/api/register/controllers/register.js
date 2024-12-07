465cbdfe434fb7dc068cffd3ea8b95ef45d87279
Sohom Ghosh <sohom.official21@gmail.com>
2024-12-07 20:03:41 +0530

    backend update

--- backend/src/api/register/controllers/register.js
+++ backend/src/api/register/controllers/register.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  category controller
+ * register controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::category.category');
+module.exports = createCoreController('api::register.register');
